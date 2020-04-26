98c4e46e785b1e27029dfe53fdd43ad3beb19ba2
Robby Emmert <robby@emmert.io>
2020-04-26 06:30:46 -0400

    poc working

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,7 @@ COPY package*.json ./
 
 RUN npm install
 
+COPY . /app
+
 EXPOSE 3000
 CMD ["npm", "run", "start:dev"]
--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return "Hello! It looks like you haven't installed your own app yet.";
   }
 }
